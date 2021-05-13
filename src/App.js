c1bc8e57345c2a12146ee06672862842feca76db
MickStick <mickstick01@hotmail.com>
2021-05-13 06:08:28 -0400

    Added employee listing and component (Menu.js)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {Nav, Navbar, NavbarBrand, NavLink} from 'reactstrap';
+import Menu from './components/menu/Menu'
+import {employees} from './shared/employees';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      employees
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Facades React.js Test</NavbarBrand>
+            <NavLink href="/about" style={{"color": "white"}}>About</NavLink>
+          </div>
+        </Navbar><br/>
         <div className="container">
-          <NavbarBrand href="/">Facades React.js Test</NavbarBrand>
-          <NavLink href="/about" style={{"color": "white"}}>About</NavLink>
+          <h1 className="center" center>Facades React.js Test</h1>
+          <h3>A Facabe Website built during a Cousera React Frontend Course</h3>
         </div>
-      </Navbar><br/>
-      <div className="container">
-        <h1 className="center">Facades React.js Test</h1>
-        <h3>A Facabe Website built during a Cousera React Frontend Course</h3>
+        <br/>
+        <Menu employees={this.state.employees}/>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,26 +1,64 @@
 import React, {Component} from  'react';
-import {Media} from 'reactstrap';
+import {Media, Card, CardImg, CardText, CardBody,CardTitle, CardSubtitle, Row, Col} from 'reactstrap';
 
 class Menu extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            dishes: [
-
-            ]
+          selectedEmp: null
         }
+
+        this.onSelectEmp().bind(this)
+    }
+
+    onSelectEmp(emp){
+      this.setState({
+        selectedEmp: emp
+      })
+    }
+    
+    renderEmp(emp){
+            if(emp != null){
+                return(
+                    <Card>
+                        <CardImg top width="100%" src={"./static/images/Facade Test Employee square.png"} alt={`${emp.first_name}_${emp.last_name}_${emp.empNumber}`} />
+                        <CardBody>
+                            <CardTitle tag="h5">{`${emp.first_name} ${emp.last_name}`}</CardTitle>
+                            <CardText>
+                                <p>{emp.bio}</p>
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                )
+            }
     }
 
     render(){
-        const {menu} = this.props;
+        const {employees} = this.props;
 
         return(
             <div className="container">
                 <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
+                    <Row>
+                        {employees ? employees.map(employee => (
+                            <Col key={employee.id} xs="6" sm="4" style={{marginBottom: "10mm"}}>
+                                <Media>
+                                    <Card onClick={() => this.onSelectEmp(employee)}>
+                                        <CardImg top width="100%" src={"./static/images/Facade Test Employee square.png"} alt={`${employee.first_name}_${employee.last_name}_${employee.empNumber}`} />
+                                        <CardBody>
+                                            <CardTitle tag="h5">{employee.role}</CardTitle>
+                                            <CardSubtitle tag="h6" className="mb-2 text-muted">{`${employee.first_name} ${employee.last_name}`} </CardSubtitle>
+                                            <CardText>
+                                                <p>{employee.email}</p>
+                                                <p>{employee.empNumber}</p>
+                                            </CardText>
+                                        </CardBody>
+                                    </Card>
+                                </Media>
+                            </Col>
+                        )): null}
+                    </Row>
                 </div>
             </div>
         );
